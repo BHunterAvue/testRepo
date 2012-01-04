e39a62b96c780a2c0768813b642b0fd62f7def1d
bhunter <bhunter@avuetech.com>
2012-01-04 08:40:10 -0800

    Initial commit.

---
+++ nested1/nested3/hello_world3.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="content-type" content="text/html;charset=UTF-8" />
+<title>Insert title here</title>
+</head>
+<body>
+
+<?php
+  print "<h1>Hello World</h1><br />";
+  print "<h2>Hello World</h2><br />";
+  print "<h3>Hello World</h3><br />";
+  print "<p>This is using Git.</p>";
+  print "<p>This is using Git too.</p>";
+?>
+
+</body>
+</html>
+
---
+++ nested2/how_now.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="content-type" content="text/html;charset=UTF-8" />
+<title>Insert title here</title>
+</head>
+<body>
+
+<?php
+  print "<h1>Hello World</h1><br />";
+  print "<h2>Hello World</h2><br />";
+  print "<h3>Hello World</h3><br />";
+  print "<p>This is using Git.</p>";
+  print "<p>This is using Git too.</p>";
+  print "<p>Now is the time for all good men to come to the aid of their country.</p>";
+  print "<p>Add a paragraph and a 'TODO' to this file.</p>";
+  print "<p>This is a paragraph added to our new branch.</p>";
+  // TODO Adding a 'to-do' task to this file.
+?>
+
+</body>
+</html>
+
